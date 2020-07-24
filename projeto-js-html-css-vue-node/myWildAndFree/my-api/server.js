2dc16a9a09b2bd32a3f496eaffddbdfe6b35fbf8
psicukiller <joao.saul0101@gmail.com>
2020-07-23 23:15:50 -0300

    criação de rotas dentro do projeto my-api concluida

--- projeto-js-html-css-vue-node/myWildAndFree/my-api/server.js
+++ projeto-js-html-css-vue-node/myWildAndFree/my-api/server.js
@@ -4,21 +4,15 @@ const requireDir = require('require-dir');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/nodeapi', {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+        useUnifiedTopology: true, 
+        useNewUrlParser: true
+    });
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
+//const Product = mongoose.model('Product');
 
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'Node-JS',
-        description: 'Teste models node',
-        url: 'www.google.com'
-    })
-
-
-    return res.send('hello world')
-});
+app.use('/api', require("./src/routes"));
 
 app.listen(3001);
