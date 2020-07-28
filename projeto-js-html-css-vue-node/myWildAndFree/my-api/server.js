ecd2956d84a47de2fd84684a651e1bbc3ae7f395
psicukiller <joao.saul0101@gmail.com>
2020-07-27 22:38:18 -0300

    api rest finalizada

--- projeto-js-html-css-vue-node/myWildAndFree/my-api/server.js
+++ projeto-js-html-css-vue-node/myWildAndFree/my-api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', {
         useUnifiedTopology: true, 
--- projeto-js-html-css-vue-node/myWildAndFree/my-api/src/controllers/ProductController.js
+++ projeto-js-html-css-vue-node/myWildAndFree/my-api/src/controllers/ProductController.js
@@ -3,8 +3,37 @@ const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res){
-        const products = await Product.find();
+        const { page } = req.query;
+        const products = await Product.paginate({},{ page, limit: 10 });
+
 
         return res.json(products);
     },
+
+    async show(req, res){
+        const products = await Product.findById(req.params.id);
+
+        return res.json(products);
+    },
+
+    async store(req, res) {
+        const products = await Product.create(req.body);
+
+        return res.json(products);
+    },
+
+    async update(req, res){
+        const products = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        return res.json(products);
+
+    },
+
+    async destroy(req, res){
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
+
+    },
+
 }
