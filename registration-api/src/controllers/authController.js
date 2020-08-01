9068a2685e27641f51589a2072559553a0de3d0b
psicukiller <joao.saul0101@gmail.com>
2020-07-31 22:34:16 -0300

    rota de login configurada com token de acesso

--- registration-api/src/controllers/authController.js
+++ registration-api/src/controllers/authController.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
+
+function generateToken(params = {}){
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400,
+    });
+} 
+
+//área de cadastro de usuários
 
 router.post("/register", async (req, res) =>{
     const { email } = req.body;
@@ -15,11 +26,35 @@ router.post("/register", async (req, res) =>{
 
         user.pass = undefined;
 
-        return res.send({ user });
+        return res.send({ 
+            user,
+            token: generateToken({ id: user.id }),
+         });
 
     }catch(err){
         return res.status(400).send({ error: 'Registration failed' })
     }
+});
+
+//área de validação de login
+
+router.post("/authenticate", async (req, res) =>{
+    const { email, pass } = req.body;
+
+    const user = await User.findOne({ email }).select('+pass');
+
+    if(!user)
+        return res.status(400).send({ error: 'User not found' });
+
+    if(!await bcrypt.compare(pass, user.pass))
+        return res.status(400).send({ error: 'Invalid password' });
+
+    user.pass = undefined;
+    
+    res.send({ 
+        user,
+        token: generateToken({ id: user.id }),
+     });
 });
 
 module.exports = app => app.use("/auth", router);
